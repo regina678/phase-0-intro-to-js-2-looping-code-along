0104f1a481f3db00d87303187e072d1b5642d68a
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-09 20:16:33 +0300

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = []; 
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`; 
+        messages.push(message); 
+    }
+
+    return messages; 
+}
+
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+const  eventName = "surprise";
+console.log(writeCards(namesArray, eventName));
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number);
+        number--
+    }
+}
+countDown(4);
+
